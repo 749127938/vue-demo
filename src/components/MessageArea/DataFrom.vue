855e966c413cac15b01b94be7dae892600923845
chuxia <749127938@qq.com>
2022-01-02 21:18:25 +0800

    新建Message组件

---
+++ src/components/MessageArea/DataFrom.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>提交留言表单</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MessageArea/DataList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>留言列表</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
